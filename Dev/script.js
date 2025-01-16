54b19eb831b5240f8f68675ca742c35ab3df6172
tututelles <59519472+tututelles@users.noreply.github.com>
2025-01-16 13:51:59 +0000

    update

--- Dev/script.js
+++ Dev/script.js
@@ -28,3 +28,28 @@ function menu2(){
         document.getElementById("but2").style.display = "none";
 }
 
+let nextButton = document.getElementsById("next");
+let prevtButton = document.getElementsById("prev");
+let carousel = document.querySelector(".carousel");
+let list = document.querySelector(".carousel .list");
+
+nextButton.onclick = function(){
+        showSlider('next');
+}
+prevButton.onclick = function(){
+        showSlider('prev');
+}
+const showSlider = (type) => {
+        document.getElementsById("next").style.pointerEvents = 'none';
+        document.getElementsById("prev").style.pointerEvents = 'none';
+        document.querySelector(".carousel").classList.remove('prev', 'next');
+        let items = document.querySelector(".carousel .list .obj")
+        if (type =='next'){
+                document.querySelector(".carousel .list").appendChild(document.querySelector(".carousel .list .obj")[0]);
+                document.querySelector(".carousel").classList.add('next');
+        }
+        else{
+                let  positionLast = items.length -1;
+                document.querySelector(".carousel .list").prepend(document.querySelector(".carousel .list .obj")[document.querySelector(".carousel .list .obj").length -1])
+        }
+}
